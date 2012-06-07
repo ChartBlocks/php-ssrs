d5fc5d659d7c6f76b07f781f253b67835f1b1dd1
arron.woods <arron.woods@deae1e92-32f9-c189-e222-5b9b5081a27a>
2012-06-07 12:25:49 +0000

    Fix report parameter mess, making it possible to send paramter values easily

--- library/SSRS/Object/ReportParameter.php
+++ library/SSRS/Object/ReportParameter.php
@@ -7,4 +7,12 @@
  */
 class SSRS_Object_ReportParameter extends SSRS_Object_Abstract {
 
+    public function __construct($name, $value) {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public $name;
+    public $value;
+
 }
