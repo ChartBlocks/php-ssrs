5942c77149cfc3cfca490c6b10ce7ed62b6b6f6f
jaysmith6811@gmail.com <jaysmith6811@gmail.com@deae1e92-32f9-c189-e222-5b9b5081a27a>
2012-10-15 15:00:22 +0000

    fixed a bug with header filename

--- library/SSRS/Object/ReportOutput.php
+++ library/SSRS/Object/ReportOutput.php
@@ -7,10 +7,10 @@
  */
 class SSRS_Object_ReportOutput extends SSRS_Object_Abstract {
 
-    public function download($filename){
+    public function download($filename) {
         header("Cache-Control: public");
         header("Content-Description: File Transfer");
-        header("Content-Disposition: attachment; filename=$filename");
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
         header("Content-Type: " . $this->mimeType);
         header("Content-Transfer-Encoding: binary");
         header("Content-Length: " . strlen($this->Result));
@@ -18,7 +18,7 @@ class SSRS_Object_ReportOutput extends SSRS_Object_Abstract {
         die($this->Result);
     }
 
-    public function __toString(){
+    public function __toString() {
         return (string) $this->Result;
     }
 
